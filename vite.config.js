a66f99a12b189b8976e40c109bc335c1de9f2146
Julia Vishnevska <julia.vishnevskaya@hobro.digital>
2026-07-30 15:28:21 +0300

    vite

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   base: '/weblow-threejs/',
+  server: {
+    host: true,
+    allowedHosts: true,
+    cors: true
+  },
   build: {
     rollupOptions: {
       output: {
